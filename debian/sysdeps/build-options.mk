740649007fdf7823cc5e0f554dc1956291339a7a
Jeff Bailey <jbailey@debian.org>
2002-10-18 15:51:33 +0000

    - debian/sysdeps/build-options.mk: Strip libc on alpha
    svn path=/trunk/; revision=94

--- debian/sysdeps/build-options.mk
+++ debian/sysdeps/build-options.mk
@@ -1,8 +1,4 @@
-ifeq ($(DEB_HOST_GNU_CPU),alpha)
-DEB_BUILD_OPTIONS = nostrip parallel
-else
 DEB_BUILD_OPTIONS = debug parallel
-endif
 
 ifneq (,$(findstring debug,$(DEB_BUILD_OPTIONS)))
 DEB_BUILD_OPTION_DEBUG = yes
