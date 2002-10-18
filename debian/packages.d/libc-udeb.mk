12497f11406d07f74c771253a1b3000a2d7da609
GOTO Masanori <gotom@debian.org>
2002-10-18 19:19:18 +0000

    - debian/packages.d/libc-udeb.mk: Clean up and fix indent crap.
    svn path=/trunk/; revision=96

--- debian/packages.d/libc-udeb.mk
+++ debian/packages.d/libc-udeb.mk
@@ -1,45 +1,40 @@
 libc-udeb: $(stamp_install) debian/control $(mkdir)/sysdeps.mk
-       $(checkroot)
-       $(debian-clean)   
-       -rm -rf $(tmpdir)/$@
+	$(checkroot)
+	$(debian-clean)
+	-rm -rf $(tmpdir)/$@
 
-       $(make_directory) $(tmpdir)/$@/lib
-       $(make_directory) $(tmpdir)/$@/DEBIAN
-       $(INSTALL_PROGRAM) $(install_root)/lib/ld-$(VERSION).so
-$(tmpdir)/$@/lib/.
-       $(INSTALL_DATA) $(install_root)/lib/lib{c,m,dl}-$(VERSION).so
-$(tmpdir)/$@/lib/.
-       $(INSTALL_PROGRAM) $(install_root)/lib/libc-$(VERSION).so
-$(tmpdir)/$@/lib/.
+	$(make_directory) $(tmpdir)/$@/lib
+	$(make_directory) $(tmpdir)/$@/DEBIAN
+	$(INSTALL_PROGRAM) $(install_root)/lib/ld-$(VERSION).so $(tmpdir)/$@/lib/.
+	$(INSTALL_DATA) $(install_root)/lib/lib{c,m,dl}-$(VERSION).so $(tmpdir)/$@/lib/.
+	$(INSTALL_PROGRAM) $(install_root)/lib/libc-$(VERSION).so $(tmpdir)/$@/lib/.
 
 ifeq ($(DEB_HOST_GNU_SYSTEM),gnu)
 # Why doesn't the glibc makefile install this?
-       ln -sf ld.so.1 $(tmpdir)/$@/lib/ld.so
+	ln -sf ld.so.1 $(tmpdir)/$@/lib/ld.so
 endif
 
 # It is safe to strip the dynamic linker though _dl_debug_state must be kept
 # in order for gdb to work.
 ifeq ($(DEB_BUILD_OPTION_STRIP),yes)
   #ifeq ($(DEB_HOST_GNU_CPU),i386) // Let's try this for all archs
-       $(STRIP) -g -K _dl_debug_state -R .note -R .comment
-$(tmpdir)/$@/lib/ld-$(VERSION).so
+	$(STRIP) -g -K _dl_debug_state -R .note -R .comment $(tmpdir)/$@/lib/ld-$(VERSION).so
   #endif
-       cd $(tmpdir)/$@ && \
-       $(STRIP) --strip-unneeded -R .note -R .comment lib/lib*-$(VERSION).so
+	cd $(tmpdir)/$@ && \
+	$(STRIP) --strip-unneeded -R .note -R .comment lib/lib*-$(VERSION).so
 endif
 
-       # Install the libs under their sonames.
-       set -ex; cd $(tmpdir)/$@/lib; \
-       for f in *.so; do \
-       mv $$f `readelf -W --dynamic $$f | grep SONAME | \
-               sed -e 's/.*\[//' -e 's/].*//'`; done
+	# Install the libs under their sonames.
+	set -ex; cd $(tmpdir)/$@/lib; \
+	for f in *.so; do \
+	mv $$f `readelf -W --dynamic $$f | grep SONAME | \
+		sed -e 's/.*\[//' -e 's/].*//'`; done
 
-       dpkg-gencontrol -isp -p$@ -P$(tmpdir)/$@ $(libc-udeb_control_flags) \
-               -DProvides="$(shell perl debian/debver2localesdep.pl \
-               $(DEBVERSION))" -fdebian/files~
-       dpkg-distaddfile libc-udeb_$(DEBVERSION)_$(shell dpkg-architecture
--qDEB_BUILD_ARCH).udeb debian-installer required
-       chown -R root.root $(tmpdir)/$@
-       chmod -R go=rX $(tmpdir)/$@
-       dpkg --build $(tmpdir)/$@ ../libc-udeb_$(DEBVERSION)_$(shell
+	dpkg-gencontrol -isp -p$@ -P$(tmpdir)/$@ $(libc-udeb_control_flags) \
+		-DProvides="$(shell perl debian/debver2localesdep.pl \
+		$(DEBVERSION))" -fdebian/files~
+	dpkg-distaddfile libc-udeb_$(DEBVERSION)_$(shell dpkg-architecture -qDEB_BUILD_ARCH).udeb debian-installer required
+	chown -R root.root $(tmpdir)/$@
+	chmod -R go=rX $(tmpdir)/$@
+	dpkg --build $(tmpdir)/$@ ../libc-udeb_$(DEBVERSION)_$(shell dpkg-architecture -qDEB_BUILD_ARCH).udeb
 
