954802b3d5315b464b6132ce5943148af375806f
GOTO Masanori <gotom@debian.org>
2003-01-15 16:40:18 +0000

    - debian/sysdeps/depflags.pl: Fix again to conflict against wine       (<< 0.0.20021007-1) and php4 (<< 4:4.2.3-5). (Close: #170385)     - debian/control: Fix unneeded Conflicts: wine and php4, as denoted above.     - debian/control.in/libc: likewise.
    svn path=/trunk/; revision=168

--- debian/sysdeps/depflags.pl
+++ debian/sysdeps/depflags.pl
@@ -123,6 +123,10 @@ push @{$libc_dev_c{'Replaces'}}, 'netkit-rpc';
 push @{$libc_c{'Replaces'}}, 'netbase (<< 4.0)';
 push @{$libc_dev_c{'Replaces'}}, 'netbase (<< 4.0)';
 
+# Conflict old wine/php4
+push @{$libc_c{'Conflicts'}}, 'wine (<< 0.0.20021007-1)';
+push @{$libc_c{'Conflicts'}}, 'php4 (<< 4:4.2.3-5)';
+
 # Make sure we only have one version of libc-dev installed
 push @{$libc_dev_c{'Provides'}}, 'libc-dev';
 push @{$libc_dev_c{'Conflicts'}}, 'libc-dev';
