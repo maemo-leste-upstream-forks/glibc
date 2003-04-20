763ddbf626b39bd7e8e3fb9318feb568cbd0fd3e
GOTO Masanori <gotom@debian.org>
2003-04-20 16:36:39 +0000

    - debian/libc/etc/init.d/devpts.sh: Modify mounting devpts for 2.5.68 and       later.  As of 2.5.68, devpts is not automounted when using devfs. So       even in that case, devpts needs to be mounted via the devpts.sh script       as well as the case that devfs is not used.  (Closes: #189792)
    svn path=/trunk/; revision=265

--- debian/libc/etc/init.d/devpts.sh
+++ debian/libc/etc/init.d/devpts.sh
@@ -10,12 +10,16 @@ set -e
 #	First find out if devpts is available. Also check if devfs
 #	is already mounted - in that case we don't want to use devpts.
 #
+#	As of 2.5.68, devpts is not automounted when using devfs.
+#	So even in that case, devpts needs to be mounted via the
+#	devpts.sh script as well as the case that devfs is not used.
+#
 devpts_avail=`grep -ci '[<[:space:]]devpts' /proc/filesystems || true`
 devpts_mounted=`grep -ci '/dev/pts' /proc/mounts || true`
 devfs_mounted=`grep -ci '[<[:space:]]/dev[>[:space:]].*devfs' /proc/mounts || true`
+kernel_ver=`uname -r`
 
-if [ "$devfs_mounted" = 0 ] && [ "$devpts_avail" != 0 ]
-then
+prepare_and_mount() {
 	#
 	#	Create mountpoint and multiplexor device.
 	#
@@ -28,5 +32,18 @@ then
 	if [ "$devpts_mounted" = 0 ]
 	then
 		mount -t devpts devpts /dev/pts -ogid=${TTYGRP},mode=${TTYMODE}
+	fi
+}
+
+if [ "$devpts_avail" != 0 ]
+then
+	if dpkg --compare-versions "$kernel_ver" ge 2.5.68
+	then
+		prepare_and_mount
+	else
+		if [ "$devfs_mounted" = 0 ]
+		then
+			prepare_and_mount
+		fi
 	fi
 fi
