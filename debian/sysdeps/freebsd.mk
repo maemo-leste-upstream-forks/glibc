757033c8481644f4ace2aaa01ed7432bbb03f2f3
GOTO Masanori <gotom@debian.org>
2003-08-29 15:14:09 +0000

    - debian/sysdeps/freebsd.mk: Modified config-os from freebsd-gnu       to kfreebsd-gnu.  Patched by Robert Millan <zeratul2@wanadoo.es>.       (Closes: #206663)
    svn path=/trunk/; revision=362

--- debian/sysdeps/freebsd.mk
+++ debian/sysdeps/freebsd.mk
@@ -1,5 +1,5 @@
 threads = yes
-config-os = freebsd-gnu
+config-os = kfreebsd-gnu
 add-ons = linuxthreads
 NO_TEST = yes
 extra_config_options := --disable-compatible-utmp --enable-kernel-include=4.6
