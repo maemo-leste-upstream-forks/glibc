4b39fe250ef941712239c99ee08f76a5379471ac
Ben Collins <bcollins@debian.org>
2004-02-03 23:35:44 +0000

    Added and enabled a sparc v9b target (UltraSPARC III).
    svn path=/trunk/; revision=640

--- debian/sysdeps/sparc.mk
+++ debian/sysdeps/sparc.mk
@@ -1,5 +1,5 @@
-GLIBC_PASSES += sparcv9 sparc64
-DEB_ARCH_REGULAR_PACKAGES += libc6-sparc64 libc6-sparcv9 libc6-dev-sparc64
+GLIBC_PASSES += sparcv9 sparcv9b sparc64
+DEB_ARCH_REGULAR_PACKAGES += libc6-sparc64 libc6-sparcv9 libc6-sparcv9b libc6-dev-sparc64
  
 sparc64_MIN_KERNEL_SUPPORTED = 2.4.18
 sparc64_configure_target=sparc64-linux
@@ -16,3 +16,10 @@ sparcv9_extra_cflags = -g1 -O3 -fomit-frame-pointer -D__USE_STRING_INLINES
 sparcv9_extra_config_options = $(extra_config_options) --disable-profile
 sparcv9_add-ons = linuxthreads $(add-ons)
 sparcv9_LIBDIR = /v9
+
+sparcv9b_MIN_KERNEL_SUPPORTED = 2.4.18
+sparcv9b_configure_target=sparcv9b-linux
+sparcv9b_extra_cflags = -g1 -O3 -fomit-frame-pointer -D__USE_STRING_INLINES
+sparcv9b_extra_config_options = $(extra_config_options) --disable-profile
+sparcv9b_add-ons = linuxthreads $(add-ons)
+sparcv9b_LIBDIR = /ultra3
