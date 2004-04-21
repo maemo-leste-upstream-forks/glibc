76c509cf8677cf1ba9c56c1fcd319598edb8a7a5
GOTO Masanori <gotom@debian.org>
2004-04-21 09:00:57 +0000

    make symlink /usr/hppa64-linux/include for libc6-dev, not libc6.
    svn path=/trunk/; revision=686

--- debian/sysdeps/hppa.mk
+++ debian/sysdeps/hppa.mk
@@ -1,8 +1,8 @@
 # hppa64 needs symlink /usr/hppa64-linux/include to /usr/include
-# (See: Bug#239020)  In future we may drop this file with supporting hppa64
+# (See: Bug#239020)  In future we may drop this file with supporting hppa64.
 
-define libc6_extra_pkg_install
+define libc6-dev_extra_pkg_install
 mkdir -p debian/$(curpass)/usr
 mkdir -p debian/$(curpass)/usr/hppa64-linux
-ln -s /usr/include debian/$(curpass)/usr/hppa64-linux/include
+ln -sf /usr/include debian/$(curpass)/usr/hppa64-linux/include
 endef
