547d296dab02e11e001152bc874140642890b63f
GOTO Masanori <gotom@debian.org>
2004-07-26 23:53:31 +0000

    - debian/patches/fno-unit-at-a-time.dpatch: Add -fno-unit-at-a-time       test to configure for amd64 + gcc-3.4.  (Closes: #261082)
    svn path=/trunk/; revision=733

--- debian/sysdeps/amd64.mk
+++ debian/sysdeps/amd64.mk
@@ -1,3 +1,11 @@
 CC = gcc
 BUILD_CC = gcc
+
 GLIBC_PASSES += nptl
+nptl_extra_cflags = -O3 -fomit-frame-pointer -D__USE_STRING_INLINES
+
+# work around patch for gcc-3.4: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=260710
+BUILD_CC_VERSION := $(shell $(BUILD_CC) -dumpversion | sed 's/\([0-9]*\.[0-9]*\)\(.*\)/\1/')
+ifeq ($(BUILD_CC_VERSION),3.4)
+	nptl_extra_cflags += -g0
+endif
