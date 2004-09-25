4ed9c149145d01923b9b826a5b06bbf5ca63d9cb
GOTO Masanori <gotom@debian.org>
2004-09-25 09:55:49 +0000

    - debian/debhelper.in/libc.preinst: Don't install glibc when kernel       is not 2.6 on amd64.     - debian/sysdeps/amd64.mk: Patch from Andreas Jochens:        - Drop the 'nptl' pass from GLIBC_PASSES.        - Use 'nptl' instead of 'linuxthreads' in the 'libc' pass (this          requires kernel >= 2.6.0, but 2.4 is not supported by the amd64          port anyway).
    svn path=/trunk/; revision=755

--- debian/sysdeps/amd64.mk
+++ debian/sysdeps/amd64.mk
@@ -1,11 +1,14 @@
 CC = gcc
 BUILD_CC = gcc
 
-GLIBC_PASSES += nptl
-nptl_extra_cflags = -O3 -fomit-frame-pointer -D__USE_STRING_INLINES
+# build libc with nptl instead of linuxthreads
+libc_MIN_KERNEL_SUPPORTED = 2.6.0
+libc_add-ons = nptl $(add-ons)
+libc_extra_cflags = -O3 -fomit-frame-pointer -D__USE_STRING_INLINES
+libc_extra_config_options := $(extra_config_options) --with-tls --with-__thread
 
 # work around patch for gcc-3.4: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=260710
 BUILD_CC_VERSION := $(shell $(BUILD_CC) -dumpversion | sed 's/\([0-9]*\.[0-9]*\)\(.*\)/\1/')
 ifeq ($(BUILD_CC_VERSION),3.4)
-	nptl_extra_cflags += -g0
+	libc_extra_cflags += -g0
 endif
