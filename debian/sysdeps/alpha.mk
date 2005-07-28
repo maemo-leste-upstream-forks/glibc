d0e21fc3c3191c23238fabc4133030e8a3c87bf9
GOTO Masanori <gotom@debian.org>
2005-07-28 02:30:11 +0000

    - debian/sysdeps/alpha.mk: Disabled TLS for static linked executables.
    svn path=/glibc-package/trunk/; revision=974

--- debian/sysdeps/alpha.mk
+++ debian/sysdeps/alpha.mk
@@ -3,3 +3,6 @@ libc = libc6.1
 # disabled because alpha linuxthreads ex7, ex9, ex10, ex18 stopps eternally
 # even if TIMEOUTSCALE is set - SIGALRM from the program is just ignored.
 RUN_TESTSUITE = no
+
+# disabled for static linked pthread programs.
+libc_extra_config_options = $(extra_config_options) --without-tls --without-__thread
