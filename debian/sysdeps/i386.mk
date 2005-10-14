a5d54079db13114402d46d58d40c2b6a2d2e9afa
Daniel Jacobowitz <dan@debian.org>
2005-10-14 02:34:05 +0000

    * Build 64-bit packages on i386 - based on patches from both Ubuntu     and Andreas Jochens <aj@andaco.de>.     - Build depend on a biarch linux-kernel-headers package.     - Build depend on new libc6-dev-amd64 package.     - Conflict with amd64-libs to avoid stale libraries.     - Replace files from amd64-libs-dev.     - Use lib64 for 64-bit libraries on i386.     - Search lib64 for ldconfig.     - Install 64-bit headers in /usr/include/x86_64-linux-gnu.
    svn path=/glibc-package/trunk/; revision=1039

--- debian/sysdeps/i386.mk
+++ debian/sysdeps/i386.mk
@@ -20,3 +20,28 @@ i686_MIN_KERNEL_SUPPORTED = 2.6.0
 i686_extra_config_options = $(extra_config_options) --disable-profile --with-tls --with-__thread
 
 libc_extra_config_options = $(extra_config_options) --with-tls --without-__thread
+
+# build 64-bit (amd64) alternative library
+GLIBC_PASSES += amd64
+DEB_ARCH_REGULAR_PACKAGES += libc6-amd64 libc6-dev-amd64
+libc6-amd64_shlib_dep = libc6-amd64 (>= $(shlib_dep_ver))
+
+amd64_configure_target = x86_64-linux
+# __x86_64__ is defined here because Makeconfig uses -undef and the
+# /usr/include/asm wrappers need that symbol.
+amd64_CC = $(CC) -m64 -D__x86_64__
+amd64_add-ons = nptl $(add-ons)
+amd64_extra_cflags = -O3 -g1
+amd64_extra_config_options = $(extra_config_options) --disable-profile \
+	--includedir=/usr/include/x86_64-linux-gnu
+amd64_LIBDIR = 64
+amd64_MIN_KERNEL_SUPPORTED = 2.6.0
+
+define amd64_extra_install
+cp debian/tmp-amd64/usr/bin/ldd debian/tmp-libc/usr/bin
+endef
+
+define libc6-dev-amd64_extra_pkg_install
+dh_install -plibc6-dev-amd64 debian/tmp-amd64/usr/include/x86_64-linux-gnu \
+	usr/include
+endef
