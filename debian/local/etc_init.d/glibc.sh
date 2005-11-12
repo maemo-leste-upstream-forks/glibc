43c8da4cc4ca0a01cb52fb6cdec99784587e6f3d
Daniel Jacobowitz <dan@debian.org>
2005-11-12 23:40:43 +0000

    * Add LSB headers to glibc.sh and nscd init scripts, from Petter     Reinholdtsen (Closes: #335308, #335343).   * Remove obsolete mountkernfs, tmpfs, and devpts.sh files from debian/.
    svn path=/glibc-package/trunk/; revision=1056

--- debian/local/etc_init.d/glibc.sh
+++ debian/local/etc_init.d/glibc.sh
@@ -1,5 +1,13 @@
 #! /bin/sh -e
 #
+### BEGIN INIT INFO
+# Provides:          glibc
+# Required-Start:
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+### END INIT INFO
+#
 # This script is existed for detecting depreciated kernel version to
 # check glibc incompatibility.
 
