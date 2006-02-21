268aaabb5e146f9fa5e3db0da662702fa20d7491
Aurelien Jarno <aurel32@debian.org>
2006-02-21 19:18:51 +0000

    * Lintian cleans:     - Use an absolute symlink for /lib64 on amd64
    svn path=/glibc-package/trunk/; revision=1201

--- debian/sysdeps/amd64.mk
+++ debian/sysdeps/amd64.mk
@@ -15,7 +15,7 @@ endif
 
 # /lib64 and /usr/lib64 are provided by glibc instead base-files: #259302.
 define libc6_extra_pkg_install
-ln -sf lib debian/$(curpass)/lib64
+ln -sf /lib debian/$(curpass)/lib64
 ln -sf lib debian/$(curpass)/usr/lib64
 endef
 
