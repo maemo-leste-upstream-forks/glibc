5c5e3fcd45774e3b2534b8718aa22754fe37689c
Aurelien Jarno <aurel32@debian.org>
2006-02-22 00:36:48 +0000

    * Remove old hack for gcc-3.4 on amd64.  * Build with the compiler defined as default for glibc (ie currently    gcc-4.0) instead of gcc on amd64.
    svn path=/glibc-package/trunk/; revision=1206

--- debian/sysdeps/amd64.mk
+++ debian/sysdeps/amd64.mk
@@ -1,17 +1,8 @@
-CC = gcc
-BUILD_CC = gcc
-
 # build libc with nptl instead of linuxthreads
 libc_MIN_KERNEL_SUPPORTED = 2.6.0
 libc_add-ons = nptl $(add-ons)
 libc_extra_cflags = -O3
 libc_extra_config_options := $(extra_config_options) --with-tls --with-__thread
-
-# work around patch for gcc-3.4: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=260710
-BUILD_CC_VERSION := $(shell $(BUILD_CC) -dumpversion | sed 's/\([0-9]*\.[0-9]*\)\(.*\)/\1/')
-ifeq ($(BUILD_CC_VERSION),3.4)
-	libc_extra_cflags += -g0
-endif
 
 # /lib64 and /usr/lib64 are provided by glibc instead base-files: #259302.
 define libc6_extra_pkg_install
