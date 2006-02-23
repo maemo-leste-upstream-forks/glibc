86395605fd9e552b7059204b5acb8e15cee3674f
Aurelien Jarno <aurel32@debian.org>
2006-02-23 11:03:27 +0000

    * Merged a patch from Michael Bank to use gcc-4.0 on Hurd.
    svn path=/glibc-package/trunk/; revision=1224

--- debian/sysdeps/hurd.mk
+++ debian/sysdeps/hurd.mk
@@ -6,10 +6,6 @@ threads = no
 libc = libc0.3
 slibdir = /lib
 
-# work around to build, gcc-3.4/gcc-4.0 are not tested yet.
-CC = gcc-3.3
-BUILD_CC = gcc-3.3
-
 # Glibc should really do this for us.
 define libc_extra_install
 mkdir -p debian/tmp-$(curpass)/lib
@@ -21,6 +17,7 @@ define kernel_check
 true
 endef
 
-# TLS is broken currently and results in Mach panicing
-libc_extra_config_options := $(extra_config_options) --without-tls
+# TLS is broken currently and results in Mach panicing.
+# ld.so is broken currently if z_relro is used.
+libc_extra_config_options := $(extra_config_options) --without-tls libc_cv_z_relro=no
 
