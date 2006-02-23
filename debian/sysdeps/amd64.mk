485d2727aad27c84a5d5b6d462de5167de943753
Aurelien Jarno <aurel32@debian.org>
2006-02-23 10:22:36 +0000

    Remove previous changes concerning the dynamic linker path on amd64, as they appear to break the installer. Use that instead:
     * Remove the patch to install the libc into /lib instead of /lib64 on
       amd64. Use the new libdir, slibdir and rtlddir options in configparm to
       install the glibc in (/usr)/lib, and to use the dynamic linker path
       specified by the ABI for path for libc objects.  (Closes: #325226)
    
    
    svn path=/glibc-package/trunk/; revision=1222

--- debian/sysdeps/amd64.mk
+++ debian/sysdeps/amd64.mk
@@ -2,22 +2,13 @@
 libc_MIN_KERNEL_SUPPORTED = 2.6.0
 libc_add-ons = nptl $(add-ons)
 libc_extra_cflags = -O3 -g1
+slibdir = /lib
+libdir = /usr/lib
+rtlddir = /lib64
 
 # /lib64 and /usr/lib64 are provided by glibc instead base-files: #259302.
 define libc6_extra_pkg_install
 ln -sf /lib debian/$(curpass)/lib64
 ln -sf lib debian/$(curpass)/usr/lib64
-endef
-define libc6-udeb_extra_pkg_install
-ln -sf /lib debian/$(curpass)/lib64
-mkdir -p debian/$(curpass)/usr/lib
-ln -sf lib debian/$(curpass)/usr/lib64
-endef
-
-define libc_extra_install
-mv debian/tmp-libc/lib64 debian/tmp-libc/lib
-mv debian/tmp-libc/usr/lib64/* debian/tmp-libc/usr/lib
-for link in $$(find debian/tmp-libc/usr/lib -type l) ; do ln -sf $$(readlink $$link | sed -e "s#64##g") $$link ; done
-for so in $$(find debian/tmp-libc/usr/lib -maxdepth 1 -type f -name *.so) ; do perl -pi -e "s#/lib64/#/lib/#g" $$so ; done
 endef
 
