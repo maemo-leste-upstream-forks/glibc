01e1896112876e454c6ae5231f2c7872f040dca3
Aurelien Jarno <aurel32@debian.org>
2006-03-18 12:08:37 +0000

    * debian/sysdeps/kfreebsd-amd64.mk:    - s/libc6/libc0.1/g    - Change rtlddir from /lib64 to /lib
    svn path=/glibc-package/trunk/; revision=1297

--- debian/sysdeps/kfreebsd-amd64.mk
+++ debian/sysdeps/kfreebsd-amd64.mk
@@ -1,9 +1,9 @@
 libc_slibdir = /lib
 libc_libdir = /usr/lib
-libc_rtlddir = /lib64
+libc_rtlddir = /lib
 
 # /lib64 and /usr/lib64 are provided by glibc instead base-files: #259302.
-define libc6_extra_pkg_install
+define libc0.1_extra_pkg_install
 ln -sf /lib debian/$(curpass)/lib64
 ln -sf lib debian/$(curpass)/usr/lib64
 endef
