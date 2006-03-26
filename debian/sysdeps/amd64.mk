39c08ed0b35a6cf9f2119a0fac5e640ba2e7c413
Aurelien Jarno <aurel32@debian.org>
2006-03-26 22:34:50 +0000

    +  * Make ldconfig look into /lib32 and /usr/lib32 on amd64. Add /lib32 -> +    /emul/ia32-linux/lib and /usr/lib32 -> /emul/ia32-linux/usr/lib links.
    svn path=/glibc-package/trunk/; revision=1308

--- debian/sysdeps/amd64.mk
+++ debian/sysdeps/amd64.mk
@@ -31,10 +31,11 @@ define libc6-dev-i386_extra_pkg_install
 mkdir -p debian/libc6-dev-i386/usr/include
 cp -af debian/tmp-i386/usr/include/i486-linux-gnu \
 	debian/libc6-dev-i386/usr/include
-ln -sf /emul/ia32-linux/usr/lib debian/libc6-dev-i386/usr/lib32
 endef
 
 define libc6-i386_extra_pkg_install
 mkdir -p debian/libc6-i386/lib
 ln -sf /emul/ia32-linux/lib/ld-linux.so.2 debian/libc6-i386/lib
+ln -sf /emul/ia32-linux/lib debian/libc6-i386/lib32
+ln -sf /emul/ia32-linux/usr/lib debian/libc6-i386/usr/lib32
 endef
