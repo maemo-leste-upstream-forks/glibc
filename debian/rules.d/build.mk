8ba5f3975bd32d2388afd1ada6ab8550b6a15428
Aurelien Jarno <aurel32@debian.org>
2006-04-11 13:33:13 +0000

    * Add beginning and end timestamps to both the build logs and the     testsuite logs.
    svn path=/glibc-package/trunk/; revision=1411

--- debian/rules.d/build.mk
+++ debian/rules.d/build.mk
@@ -57,6 +57,7 @@ $(stamp)configure_%: $(stamp)mkbuilddir_%
 	    echo "No.  Forcing cross-compile by setting build to $$configure_build."; \
 	  fi; \
 	fi; \
+	$(call logme, -a $(log_build), echo -n "Build started: " ; date ; echo "---------------")
 	$(call logme, -a $(log_build), \
 		cd $(DEB_BUILDDIR) && \
 		CC="$(call xx,CC)" \
@@ -67,13 +68,13 @@ $(stamp)configure_%: $(stamp)mkbuilddir_%
 		--enable-add-ons=$(standard-add-ons)"$(call xx,add-ons)" \
 		--without-selinux \
 		$(call xx,with_headers) $(call xx,extra_config_options))
-
 	touch $@
 
 $(patsubst %,build_%,$(GLIBC_PASSES)) :: build_% : $(stamp)build_%
 $(stamp)build_%: $(stamp)configure_%
 	@echo Building $(curpass)
 	$(call logme, -a $(log_build), $(MAKE) -C $(DEB_BUILDDIR) -j $(NJOBS))
+	$(call logme, -a $(log_build), echo "---------------" ; echo -n "Build ended: " ; date)
 	touch $@
 
 $(patsubst %,check_%,$(GLIBC_PASSES)) :: check_% : $(stamp)check_%
@@ -89,7 +90,13 @@ $(stamp)check_%: $(stamp)build_%
 	  echo "Testsuite disabled for $(curpass), skipping tests."; \
 	else \
 	  echo Testing $(curpass); \
+	  echo -n "Testsuite started: " | tee -a $(log_test); \
+	  date | tee -a $(log_test); \
+	  echo "--------------" | tee -a $(log_test); \
 	  $(MAKE) -C $(DEB_BUILDDIR) -j $(NJOBS) -k check 2>&1 | tee -a $(log_test); \
+	  echo "--------------" | tee -a $(log_test); \
+	  echo -n "Testsuite ended: " | tee -a $(log_test); \
+	  date | tee -a $(log_test); \
 	fi
 	touch $@
 
