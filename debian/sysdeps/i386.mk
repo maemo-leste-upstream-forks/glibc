d4c45f63fe7834119223f66d304710a2385d9b0f
Aurelien Jarno <aurel32@debian.org>
2006-05-21 18:12:02 +0000

    * debian/sysdeps/i386.mk: Build back the glibc with --without-__thread on     i386 (Closes: #368022, #368326).
    svn path=/glibc-package/trunk/; revision=1499

--- debian/sysdeps/i386.mk
+++ debian/sysdeps/i386.mk
@@ -1,14 +1,13 @@
-GLIBC_PASSES += nptl i686 xen
-DEB_ARCH_REGULAR_PACKAGES += libc6-i686 libc6-xen
-
 udeb_MIN_KERNEL_SUPPORTED = 2.4.1
 
 # Starting with libc6 2.3.6-6 TLS support is enabled
 libc6_shlib_dep = libc6 (>= 2.3.6-6)
+libc_extra_config_options = $(extra_config_options) --without-__thread
 
 # NPTL requires at least i486 assembly.  We don't need to take
 # special measures for i386 systems, since Debian kernel images now
 # emulate the missing instructions on the i386.
+GLIBC_PASSES += nptl
 nptl_configure_target=i486-linux
 nptl_configure_build=i486-linux
 nptl_extra_cflags = -march=i486 -mtune=i686 -g1 -O3
@@ -16,6 +15,8 @@ nptl_extra_cflags = -march=i486 -mtune=i686 -g1 -O3
 # We use -march=i686 and glibc's i686 routines use cmov, so require it.
 # A Debian-local glibc patch adds cmov to the search path.
 # The optimized libraries also use NPTL!
+GLIBC_PASSES += i686
+DEB_ARCH_REGULAR_PACKAGES += libc6-i686
 i686_add-ons = nptl $(add-ons)
 i686_configure_target=i686-linux
 i686_extra_cflags = -march=i686 -mtune=i686 -g1 -O3
@@ -26,6 +27,8 @@ i686_extra_config_options = $(extra_config_options) --disable-profile
 
 # We use -mno-tls-direct-seg-refs to not wrap-around segments, as it
 # greatly reduce the speed when running under the Xen hypervisor.
+GLIBC_PASSES += xen
+DEB_ARCH_REGULAR_PACKAGES += libc6-xen
 xen_add-ons = nptl $(add-ons)
 xen_configure_target=i686-linux
 xen_extra_cflags = -march=i686 -mtune=i686 -g1 -O3 -mno-tls-direct-seg-refs
@@ -38,7 +41,6 @@ xen_extra_config_options = $(extra_config_options) --disable-profile
 GLIBC_PASSES += amd64
 DEB_ARCH_REGULAR_PACKAGES += libc6-amd64 libc6-dev-amd64
 libc6-amd64_shlib_dep = libc6-amd64 (>= $(shlib_dep_ver))
-
 amd64_configure_target = x86_64-linux
 # __x86_64__ is defined here because Makeconfig uses -undef and the
 # /usr/include/asm wrappers need that symbol.
