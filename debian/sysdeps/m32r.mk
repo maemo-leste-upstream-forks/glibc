71ba45ae9f1cdc15554245e85717c0efe0a62fc3
Aurelien Jarno <aurel32@debian.org>
2006-05-28 09:37:48 +0000

    The port tarball is actually extracted into ports/
    svn path=/glibc-package/trunk/; revision=1547

--- debian/sysdeps/m32r.mk
+++ debian/sysdeps/m32r.mk
@@ -1,3 +1,3 @@
 libc_MIN_KERNEL_SUPPORTED = 2.6.9
-libc_add-ons = glibc-ports-2.3.6 linuxthreads $(add-ons)
+libc_add-ons = ports linuxthreads $(add-ons)
 libc_extra_config_options = $(extra_config_options) --without-tls
