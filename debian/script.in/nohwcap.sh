3baed4909daed85bba0185e5517e1708874bca72
Aurelien Jarno <aurel32@debian.org>
2007-04-17 09:36:45 +0000

    Improve the comments of script.in/nohwcap.sh
    svn path=/glibc-package/trunk/; revision=2084

--- debian/script.in/nohwcap.sh
+++ debian/script.in/nohwcap.sh
@@ -1,6 +1,11 @@
     # Handle upgrades when libc-opt package has been installed.
+    # When a /etc/ld.so.nohwcap file exists, ld.so only use libraries
+    # from /lib, and ignore all optimised libraries. This file is
+    # inconditionaly created in the preinst script of libc.
  
     # Get the list of optimized packages for a given architecture
+    # Before removing a package from this list, make sure it appears
+    # in the Conflicts: line of libc.
     case $(dpkg --print-architecture) in
         i386)
             hwcappkgs="libc6-i686 libc6-xen"
@@ -14,8 +19,8 @@
     esac
  
     # We check the version between the current installed libc and
-    # all optimized packages. If they're unmatched, we keep /etc/ld.so.nohwcap
-    # file until all optimized packages are installed or removed.
+    # all optimized packages (on architectures where such packages
+    # exists).
     all_upgraded=yes
     if [ -n "$hwcappkgs" ]; then
         for pkg in $hwcappkgs ; do
@@ -25,6 +30,10 @@
             fi
         done
     fi
+
+    # If the versions of all optimized packages are the same as the libc
+    # one, we could remove /etc/ld.so.nohwcap. Otherwise, it will be removed
+    # when all optimized packages are upgraded or removed.
     if [ "$all_upgraded" = yes ] ; then
         rm -f /etc/ld.so.nohwcap
     fi
