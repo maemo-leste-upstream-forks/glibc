5d45e8bea13d46671dc9b679e4bc7e0c6135a4e1
Aurelien Jarno <aurel32@debian.org>
2007-04-23 22:51:17 +0000

    * debian/script.in/kernelcheck.sh: improve the kernel error message.
    svn path=/glibc-package/trunk/; revision=2119

--- debian/script.in/kernelcheck.sh
+++ debian/script.in/kernelcheck.sh
@@ -57,9 +57,9 @@ exit_check () {
     then
 	if kernel_compare_versions "$kernel_ver" lt 2.6.1
 	then
-	    echo WARNING: POSIX threads library NPTL requires 2.6.1 and
-	    echo later kernel.  If you use 2.4 kernel, please upgrade your
-	    echo kernel before installing glibc.
+	    echo WARNING: POSIX threads library NPTL requires kernel version
+	    echo 2.6.1 or later.  If you use a kernel 2.4, please upgrade it
+	    echo before installing glibc.
 	    exit_check
 	fi
     fi
