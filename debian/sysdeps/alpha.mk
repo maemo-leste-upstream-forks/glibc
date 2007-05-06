31069d11b99994e50dded1b2bf58dc1410eaea1b
Aurelien Jarno <aurel32@debian.org>
2007-05-06 10:49:51 +0000

    Remove old comments
    svn path=/glibc-package/trunk/; revision=2182

--- debian/sysdeps/alpha.mk
+++ debian/sysdeps/alpha.mk
@@ -1,5 +1,1 @@
 libc = libc6.1
-
-# disabled because alpha linuxthreads ex7, ex9, ex10, ex18 stopps eternally
-# even if TIMEOUTSCALE is set - SIGALRM from the program is just ignored.
-#RUN_TESTSUITE = no
