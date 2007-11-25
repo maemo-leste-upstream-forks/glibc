819df19775748647540e9ef8b335501bd730b6e2
Aurelien Jarno <aurel32@debian.org>
2007-11-25 23:14:28 +0000

    [ Aurelien Jarno ]   * sysdeps/alpha.mk: define alphaev67_MIN_KERNEL_SUPPORTED to 2.6.9.     Closes: #452890.
    svn path=/glibc-package/trunk/; revision=2697

--- debian/sysdeps/alpha.mk
+++ debian/sysdeps/alpha.mk
@@ -6,6 +6,7 @@ libc_MIN_KERNEL_SUPPORTED = 2.6.9
 # build an ev67 optimized library
 GLIBC_PASSES += alphaev67
 DEB_ARCH_REGULAR_PACKAGES += libc6.1-alphaev67
+alphaev67_MIN_KERNEL_SUPPORTED = 2.6.9
 alphaev67_add-ons = nptl $(add-ons)
 alphaev67_configure_target = alphaev67-linux
 alphaev67_extra_cflags = -mcpu=ev67 -mtune=ev67 -g -O2
