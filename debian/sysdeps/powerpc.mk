a799a8dce29488d2bdbb2aa3446022cb1d6531c1
Aurelien Jarno <aurel32@debian.org>
2008-02-27 20:43:56 +0000

    * control.in/main, sysdeps/powerpc.mk, sysdeps/ppc64.mk: use gcc-4.3     on powerpc and ppc64.
    svn path=/glibc-package/trunk/; revision=2833

--- debian/sysdeps/powerpc.mk
+++ debian/sysdeps/powerpc.mk
@@ -1,3 +1,8 @@
+ifeq ($(DEB_HOST_ARCH),$(DEB_BUILD_ARCH))
+CC     = gcc-4.3
+CXX    = g++-4.3
+endif
+
 # build 64-bit (ppc64) alternative library
 GLIBC_PASSES += ppc64
 DEB_ARCH_REGULAR_PACKAGES += libc6-ppc64 libc6-dev-ppc64
