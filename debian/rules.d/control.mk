e3c5b9a1b5a2f7a2f3bb891b167f07a2351b181a
Aurelien Jarno <aurel32@debian.org>
2008-03-18 19:52:43 +0000

    * control.in/main, rules.d/control.mk: remove @threads_arches@, as all     architectures now support threads.
    svn path=/glibc-package/trunk/; revision=2877

--- debian/rules.d/control.mk
+++ debian/rules.d/control.mk
@@ -1,7 +1,5 @@
 control_deps := $(addprefix debian/control.in/, libc6 libc6.1 libc0.1 libc0.3 sparc64 s390x powerpc ppc64 opt amd64 mipsn32 mips64)
 
-threads_archs := alpha amd64 arm armeb armel i386 m32r m68k mips mipsel powerpc ppc64 sparc ia64 hppa s390 sh3 sh4 sh3eb sh4eb kfreebsd-i386 kfreebsd-amd64 
-
 debian/control.in/libc6: debian/control.in/libc debian/rules.d/control.mk
 	sed -e 's%@libc@%libc6%g' \
 	    -e 's%@archs@%amd64 arm armeb armel i386 m32r m68k mips mipsel powerpc ppc64 sparc s390 hppa sh3 sh4 sh3eb sh4eb%g' < $< > $@
@@ -41,7 +39,6 @@ $(stamp)control: debian/control.in/main $(control_deps) \
 	cat debian/control.in/opt		>> $@T
 	cat debian/control.in/libnss-dns-udeb	>> $@T
 	cat debian/control.in/libnss-files-udeb	>> $@T
-	sed -e 's%@libc@%$(libc)%g;s%@glibc@%glibc%g' \
-	    -e 's%@threads_archs@%$(threads_archs)%g' < $@T > debian/control
+	sed -e 's%@libc@%$(libc)%g;s%@glibc@%glibc%g' < $@T > debian/control
 	rm $@T
 	touch $@
