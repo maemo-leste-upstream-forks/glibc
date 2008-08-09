f7e7ba48bf05157de62acc0c5f6feeb4d046d757
Aurelien Jarno <aurel32@debian.org>
2008-08-09 22:08:48 +0000

    Fix patch/unpatch rules for architectures specific
    svn path=/glibc-package/branches/glibc-2.8/; revision=3068

--- debian/rules.d/quilt.mk
+++ debian/rules.d/quilt.mk
@@ -13,9 +13,9 @@ $(stamp)patch:
 	  pc=".pc.$(DEB_HOST_ARCH)"; \
 	  mkdir -p "$$pc"; \
 	  ln -sf ../debian/patches/series.$(DEB_HOST_ARCH) $$pc/series; \
+	  QUILT_PC="$$pc" $(QUILT) upgrade || true; \
 	  if QUILT_PC="$$pc" $(QUILT) next >/dev/null 2>&1; then \
 	    echo "Applying architecture specific patches... "; \
-	    QUILT_PC="$$pc" $(QUILT) upgrade || true; \
 	    QUILT_PC="$$pc" $(QUILT) push -a -v ; \
 	  fi ; \
 	fi ; \
@@ -33,9 +33,9 @@ unpatch:
 	rm -rf .pc ; \
 	if test -r debian/patches/series.$(DEB_HOST_ARCH); then \
 	  pc=".pc.$(DEB_HOST_ARCH)"; \
+	  QUILT_PC="$$pc" $(QUILT) upgrade || true; \
 	  if QUILT_PC="$$pc" $(QUILT) previous >/dev/null 2>&1; then \
 	    echo "Unapplying architecture specific patches..."; \
-	    QUILT_PC="$$pc" $(QUILT) upgrade || true; \
 	    QUILT_PC="$$pc" $(QUILT) pop -a -v ; \
 	  fi ; \
 	  rm -rf $$pc ; \
