46e6c1bbdfe74288233cd792b2a54da8b0b96b6d
Aurelien Jarno <aurel32@debian.org>
2008-11-26 10:49:07 +0000

    [ Aurelien Jarno ]   * New upstream release.     - This version has been tagged in the CVS, update debian/rules and       debian/rules.d/tarball.mk accordingly.
    svn path=/glibc-package/branches/glibc-2.9/; revision=3200

--- debian/rules.d/tarball.mk
+++ debian/rules.d/tarball.mk
@@ -1,15 +1,14 @@
 GLIBC_PSERVER = :pserver:anoncvs@sources.redhat.com:/cvs/glibc
-GLIBC_BRANCH = glibc-$(subst .,_,$(GLIBC_VERSION))-branch
-GLIBC_SNAPSHOT = $(GLIBC_VERSION)+$(shell date "+%Y%m%d")
-GLIBC_DIR = glibc-$(GLIBC_SNAPSHOT)
-DEB_ORIG = ../glibc_$(GLIBC_SNAPSHOT).orig.tar.gz
+GLIBC_TAG = glibc-$(subst .,_,$(GLIBC_VERSION))
+GLIBC_DIR = glibc-$(GLIBC_VERSION)
+DEB_ORIG = ../glibc_$(GLIBC_VERSION).orig.tar.gz
 
 get-orig-source: $(DEB_ORIG)
 $(DEB_ORIG):
-	cvs -z 9 -d $(GLIBC_PSERVER) export -d $(GLIBC_DIR) -r $(GLIBC_BRANCH) libc ; \
+	cvs -z 9 -d $(GLIBC_PSERVER) export -d $(GLIBC_DIR) -r $(GLIBC_TAG) libc ; \
 	cd $(GLIBC_DIR) ; \
 	rm -fr manual/ ; \
-	cvs -z 9 -d $(GLIBC_PSERVER) export -d ports -r $(GLIBC_BRANCH) ports ; \
+	cvs -z 9 -d $(GLIBC_PSERVER) export -d ports -r $(GLIBC_TAG) ports ; \
 	cvs -z 9 -d $(GLIBC_PSERVER) export -d linuxthreads -r HEAD linuxthreads/linuxthreads ; \
 	cvs -z 9 -d $(GLIBC_PSERVER) export -d linuxthreads_db -r HEAD linuxthreads/linuxthreads_db ; \
 	cd .. ; \
