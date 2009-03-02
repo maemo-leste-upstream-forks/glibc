a31b37b25991dd38a5859f2c78c20190cbc7a182
Aurelien Jarno <aurel32@debian.org>
2009-03-02 10:24:51 +0000

    Cosmetic change: build the biarch version after the optimized flavour on sparc
    svn path=/glibc-package/branches/eglibc-2.9/; revision=3361

--- debian/sysdeps/sparc.mk
+++ debian/sysdeps/sparc.mk
@@ -1,3 +1,14 @@
+# build a sparcv9b optimized library
+EGLIBC_PASSES += sparcv9b
+DEB_ARCH_REGULAR_PACKAGES += libc6-sparcv9b
+sparcv9b_add-ons = nptl $(add-ons)
+sparcv9b_configure_target=sparcv9b-linux
+sparcv9b_configure_build=sparcv9b-linux
+sparcv9b_extra_cflags = -mtune=ultrasparc3 -O3
+sparcv9b_extra_config_options = $(extra_config_options) --disable-profile
+sparcv9b_rtlddir = /lib
+sparcv9b_slibdir = /lib/ultra3
+
 # build 64-bit (sparc64) alternative library
 EGLIBC_PASSES += sparc64
 DEB_ARCH_REGULAR_PACKAGES += libc6-sparc64 libc6-dev-sparc64
@@ -18,14 +29,3 @@ cp -af debian/tmp-sparc64/usr/include/sparc64-linux-gnu/gnu/stubs-64.h \
         debian/libc6-dev-sparc64/usr/include/gnu
 mkdir -p debian/libc6-dev-sparc64/usr/include/sparc64-linux-gnu
 endef
-
-# build a sparcv9b optimized library
-EGLIBC_PASSES += sparcv9b
-DEB_ARCH_REGULAR_PACKAGES += libc6-sparcv9b
-sparcv9b_add-ons = nptl $(add-ons)
-sparcv9b_configure_target=sparcv9b-linux
-sparcv9b_configure_build=sparcv9b-linux
-sparcv9b_extra_cflags = -mtune=ultrasparc3 -O3
-sparcv9b_extra_config_options = $(extra_config_options) --disable-profile
-sparcv9b_rtlddir = /lib
-sparcv9b_slibdir = /lib/ultra3
