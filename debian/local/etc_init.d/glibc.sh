358dab7073dae63759c3b049241ffa6288600437
Aurelien Jarno <aurel32@debian.org>
2009-07-25 22:54:06 +0000

    * Add X-Interactive: true to /etc/init.d/glibc.sh.  Closes: #538435.
    svn path=/glibc-package/trunk/; revision=3660

--- debian/local/etc_init.d/glibc.sh
+++ debian/local/etc_init.d/glibc.sh
@@ -6,6 +6,7 @@
 # Required-Stop:
 # Default-Start:     S
 # Default-Stop:
+# X-Interactive:     true
 # Short-Description: check for deprecated kernel versions
 # Description:       This script detects deprecated kernel versions incompatible with
 #                    the current version of the glibc
