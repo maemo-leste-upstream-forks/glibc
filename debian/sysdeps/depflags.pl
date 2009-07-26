9bf9c83622daa7534fe8adfa8940c9f548ce3f4d
Aurelien Jarno <aurel32@debian.org>
2009-07-26 15:14:00 +0000

    * debhelper.in/libc.postinst, sysdeps/depflags.pl: remove upgrade code     for pre-etch installations.
    svn path=/glibc-package/trunk/; revision=3665

--- debian/sysdeps/depflags.pl
+++ debian/sysdeps/depflags.pl
@@ -29,13 +29,10 @@ if ($DEB_HOST_ARCH_OS eq "hurd") {
 if ($DEB_HOST_ARCH_OS eq "linux") {
     push @{$libc_c{'Suggests'}}, 'locales';
     push @{$libc_dev_c{'Recommends'}}, 'c-compiler';
-    push @{$libc_dev_c{'Replaces'}}, ('man-db (<= 2.3.10-41)', 'gettext (<= 0.10.26-1)',
-		'ppp (<= 2.2.0f-24)', 'libgdbmg1-dev (<= 1.7.3-24)');
     push @{$libc_dev_c{'Depends'}}, 'linux-libc-dev';
 }
 if ($DEB_HOST_ARCH_OS eq "kfreebsd") {
     push @{$libc_c{'Suggests'}}, 'locales';
-    push @{$libc_c{'Replaces'}}, 'libc0.1-dev (<< 2.3.2.ds1-14)';
     push @{$libc_dev_c{'Recommends'}}, 'c-compiler';
     push @{$libc_dev_c{'Replaces'}}, 'kfreebsd-kernel-headers (<< 0.11)';
     push @{$libc_dev_c{'Depends'}}, 'kfreebsd-kernel-headers (>= 0.11)';
@@ -49,19 +46,6 @@ push @{$libc_dev_c{'Suggests'}}, "manpages-dev";
 # nss-db is now seperate
 push @{$libc_c{'Recommends'}}, 'libnss-db';
 
-# 2.1.94 required a patch, applied in gcc -15, so c++ compiles will work again
-push @{$libc_dev_c{'Conflicts'}}, 'libstdc++2.10-dev (<< 1:2.95.2-15)';
-
-# 2.2.2+CVS requires a newer gcc. For non-i386, we just worry about the
-# weak-sym patch, since on i386 we need an even newer one because of the
-# pic-kludge that breaks libc_nonshared.a inclusion.
-if ($DEB_HOST_ARCH =~ m/^i386$/) {
-    push @{$libc_dev_c{'Conflicts'}}, 'gcc-2.95 (<< 1:2.95.3-9)';
-    push @{$libc_c{'Suggests'}}, 'libc6-i686';
-} else {
-    push @{$libc_dev_c{'Conflicts'}}, 'gcc-2.95 (<< 1:2.95.3-8)';
-}
-
 ## Conflict versions of binutils version that does not support DT_GNU_HASH
 push @{$libc_dev_c{'Conflicts'}}, 'binutils (<< 2.17cvs20070426-1)';
 
@@ -71,9 +55,6 @@ push @{$libc_dev_c{'Conflicts'}}, 'libc-dev';
 if ($libc ne "libc6") {
     push @{$libc_dev_c{'Provides'}}, 'libc6-dev';
 }
-
-# Conflict with broken libterm-readline-gnu-perl to avoid #326856.
-push @{$libc_c{'Conflicts'}}, 'libterm-readline-gnu-perl (<< 1.15-2)';
 
 # Conflict with older versions of tzdata that need tzconfig.
 push @{$libc_c{'Conflicts'}}, 'tzdata (<< 2007k-1), tzdata-etch';
