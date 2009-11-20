6c51be4f6dc1b4cba00ceff44d3ea31ce8aa90b3
Aurelien Jarno <aurel32@debian.org>
2009-11-20 15:38:22 +0000

    Also reenable PIE on mipsel
    svn path=/glibc-package/trunk/; revision=3996

--- debian/sysdeps/mipsel.mk
+++ debian/sysdeps/mipsel.mk
@@ -1,9 +1,6 @@
 CC = gcc-4.3
 CXX = g++-4.3
 
-# PIE support is currently broken on MIPSEL
-fpie = no
-
 #
 # Temporary hack to use our own version of unistd.h
 # to workaround kernel bugs on the build daemons
