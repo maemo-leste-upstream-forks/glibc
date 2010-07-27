3c1e5daa347d5d4fdbe522d23a37b869fd4dcc2e
Aurelien Jarno <aurel32@debian.org>
2010-07-27 03:15:09 +0000

    * Look for apache2.2-common instead of apache2-common in nsscheck.     Closes: #586527.
    svn path=/glibc-package/trunk/; revision=4366

--- debian/script.in/nsscheck.sh
+++ debian/script.in/nsscheck.sh
@@ -3,7 +3,7 @@
 	    check=$(dpkg -s $check 2> /dev/null | egrep '^Package:|^Status:' | awk '{if ($1 ~ /^Package:/) { package=$2 } else if ($0 ~ /^Status: .* installed$/) { print package }}')
 	    # some init scripts don't match the package names
 	    check=$(echo $check | \
-	    	    sed -e's/\bapache2-common\b/apache2/g' \
+	    	    sed -e's/\bapache2.2-common\b/apache2/g' \
 	    	    	-e's/\bat\b/atd/g' \
 	    	    	-e's/\bdovecot-common\b/dovecot/g' \
 	    	    	-e's/\bexim4-base\b/exim4/g' \
