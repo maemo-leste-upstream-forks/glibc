638166a79c7d26450c4d57b8c94b47df49665d29
Aurelien Jarno <aurel32@debian.org>
2011-05-11 21:03:05 +0000

    * debian/local/memcpy-wrapper/*: on amd64, provide and build two wrappers     to workaround and debug memcpy() issues. Explain how to use them in     debian/debhelper.in/libc.NEWS.
    svn path=/glibc-package/trunk/; revision=4654

--- debian/sysdeps/amd64.mk
+++ debian/sysdeps/amd64.mk
@@ -21,6 +21,17 @@ i386_includedir = /usr/include/i486-linux-gnu
 i386_slibdir = /lib32
 i386_libdir = /usr/lib32
 
+define libc6_extra_pkg_install
+make -C debian/local/memcpy-wrapper
+install -m 755 -o root -g root -d debian/libc6/$(libdir)/libc
+install -m 755 -o root -g root \
+	debian/local/memcpy-wrapper/memcpy-preload.so \
+	debian/libc6/$(libdir)/libc
+install -m 755 -o root -g root \
+	debian/local/memcpy-wrapper/memcpy-syslog-preload.so \
+	debian/libc6/$(libdir)/libc
+endef
+
 define libc6-dev-i386_extra_pkg_install
 mkdir -p debian/libc6-dev-i386/usr/include/gnu
 cp -af debian/tmp-i386/usr/include/i486-linux-gnu/gnu/stubs-32.h \
