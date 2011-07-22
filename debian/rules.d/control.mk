e86e5dbf475c2ff9fe7e7a354480ef3449022187
Aurelien Jarno <aurel32@debian.org>
2011-07-22 11:09:52 +0000

    * Add support for s390x.
    svn path=/glibc-package/trunk/; revision=4805

--- debian/rules.d/control.mk
+++ debian/rules.d/control.mk
@@ -1,8 +1,8 @@
-control_deps := $(addprefix debian/control.in/, libc6 libc6.1 libc0.1 libc0.3 sparc64 s390x powerpc ppc64 opt amd64 mipsn32 mips64)
+control_deps := $(addprefix debian/control.in/, libc6 libc6.1 libc0.1 libc0.3 sparc64 s390 s390x powerpc ppc64 opt amd64 mipsn32 mips64)
 
 debian/control.in/libc6: debian/control.in/libc debian/rules.d/control.mk
 	sed -e 's%@libc@%libc6%g' \
-	    -e 's%@archs@%amd64 arm armeb armel armhf i386 m32r m68k mips mipsel powerpc powerpcspe ppc64 sparc sparc64 s390 hppa sh3 sh4 sh3eb sh4eb%g' < $< > $@
+	    -e 's%@archs@%amd64 arm armeb armel armhf i386 m32r m68k mips mipsel powerpc powerpcspe ppc64 sparc sparc64 s390 s390x hppa sh3 sh4 sh3eb sh4eb%g' < $< > $@
 
 debian/control.in/libc6.1: debian/control.in/libc debian/rules.d/control.mk
 	sed -e 's%@libc@%libc6.1%g;s%@archs@%alpha ia64%g' < $< > $@
@@ -29,6 +29,7 @@ $(stamp)control: debian/control.in/main $(control_deps) \
 	cat debian/control.in/libc0.1		>> $@T
 	cat debian/control.in/i386		>> $@T
 	cat debian/control.in/sparc64		>> $@T
+	cat debian/control.in/s390 		>> $@T
 	cat debian/control.in/s390x		>> $@T
 	cat debian/control.in/amd64		>> $@T
 	cat debian/control.in/powerpc		>> $@T
---
+++ debian/sysdeps/s390x.mk
@@ -0,0 +1,30 @@
+libc_rtlddir = /lib32
+
+# /lib32 and /usr/lib32 are provided by glibc instead base-files: #259302.
+define libc6_extra_pkg_install
+ln -sf /lib debian/$(curpass)/lib32
+ln -sf lib debian/$(curpass)/usr/lib32
+
+# build 32-bit (s390) alternative library
+EGLIBC_PASSES += s390
+DEB_ARCH_REGULAR_PACKAGES += libc6-s390 libc6-dev-s390
+s390_add-ons = nptl $(add-ons)
+s390_configure_target = s390-linux-gnu
+s390_CC = $(CC) -m32
+s390_CXX = $(CXX) -m32
+s390_extra_config_options := $(extra_config_options) --disable-profile
+s390_includedir = /usr/include/s390-linux-gnu
+s390_slibdir = /lib32
+s390_libdir = /usr/lib32
+
+define libc6-dev-s390_extra_pkg_install
+mkdir -p debian/libc6-dev-s390/usr/include/gnu
+cp -af debian/tmp-s390/usr/include/s390-linux-gnu/gnu/stubs-32.h \
+        debian/libc6-dev-s390/usr/include/gnu
+mkdir -p debian/libc6-dev-s390/usr/include/s390-linux-gnu
+endef
+
+define libc6-s390_extra_pkg_install
+mkdir -p debian/$(curpass)/lib
+ln -s /lib32/ld.so.1 debian/$(curpass)/lib
+endef
