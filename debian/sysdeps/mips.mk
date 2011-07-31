bd54c335b97670986d72af99990895233f87a071
Aurelien Jarno <aurel32@debian.org>
2011-07-31 00:42:54 +0000

    * sysdeps/mips.*mk: fix triarch includes on mips and mipsel.
    svn path=/glibc-package/trunk/; revision=4832

--- debian/sysdeps/mips.mk
+++ debian/sysdeps/mips.mk
@@ -13,6 +13,19 @@ mipsn32_slibdir = /lib32
 mipsn32_libdir = /usr/lib32
 mipsn32_extra_config_options := $(extra_config_options) --disable-profile
 
+define libc6-dev-mipsn32_extra_pkg_install
+
+mkdir -p debian/libc6-dev-mipsn32/usr/include
+ln -s mips-linux-gnu/bits debian/libc6-dev-mipsn32/usr/include/
+ln -s mips-linux-gnu/gnu debian/libc6-dev-mipsn32/usr/include/
+
+mkdir -p debian/libc6-dev-mipsn32/usr/include/sys
+for i in `ls debian/tmp-libc/usr/include/mips-linux-gnu/sys` ; do \
+        ln -s ../mips-linux-gnu/sys/$$i debian/libc6-dev-mipsn32/usr/include/sys/$$i ; \
+done
+
+endef
+
 # build 64-bit alternative library
 EGLIBC_PASSES += mips64
 DEB_ARCH_REGULAR_PACKAGES += libc6-mips64 libc6-dev-mips64
@@ -25,6 +38,19 @@ mips64_rtlddir = /lib64
 mips64_slibdir = /lib64
 mips64_libdir = /usr/lib64
 mips64_extra_config_options := $(extra_config_options) --disable-profile
+
+define libc6-dev-mips64_extra_pkg_install
+
+mkdir -p debian/libc6-dev-mips64/usr/include
+ln -s mips-linux-gnu/bits debian/libc6-dev-mips64/usr/include/
+ln -s mips-linux-gnu/gnu debian/libc6-dev-mips64/usr/include/
+
+mkdir -p debian/libc6-dev-mips64/usr/include/sys
+for i in `ls debian/tmp-libc/usr/include/mips-linux-gnu/sys` ; do \
+        ln -s ../mips-linux-gnu/sys/$$i debian/libc6-dev-mips64/usr/include/sys/$$i ; \
+done
+
+endef
 
 # Need to put a tri-arch aware version of ldd in the base package
 define mipsn32_extra_install
