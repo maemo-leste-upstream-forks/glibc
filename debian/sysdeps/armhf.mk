8fdb3c66b7640ec4a396da46ad6783fca5111fab
Aurelien Jarno <aurel32@debian.org>
2011-08-04 07:41:35 +0000

    * Build with gcc-4.6/g++-4.6 on armhf to get correct atomic operations in     thumb-2 mode.
    svn path=/glibc-package/trunk/; revision=4847

--- debian/sysdeps/armhf.mk
+++ debian/sysdeps/armhf.mk
@@ -1,1 +1,11 @@
 libc_add-ons = ports nptl $(add-ons)
+
+# Use gcc-4.6 on armhf to get correct atomic operations in thumb-2 mode
+ifneq ($(DEB_HOST_ARCH),$(DEB_BUILD_ARCH))
+CC     = $(DEB_HOST_GNU_TYPE)-gcc
+CXX    = $(DEB_HOST_GNU_TYPE)-g++
+else
+CC     = gcc-4.6
+CXX    = g++-4.6
+endif
+
