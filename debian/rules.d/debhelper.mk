0cfcb504375d6aae0b0f81cf64f57e8bc646ed38
Adam Conrad <adconrad@0c3.net>
2012-10-26 00:34:46 +0000

    Port over ldconfig trigger support from Ubuntu packages
    svn path=/glibc-package/branches/eglibc-2.16/; revision=5338

--- debian/rules.d/debhelper.mk
+++ debian/rules.d/debhelper.mk
@@ -33,6 +33,13 @@ $(patsubst %,$(stamp)binaryinst_%,$(DEB_ARCH_REGULAR_PACKAGES) $(DEB_INDEP_REGUL
 	dh_link -p$(curpass)
 	dh_bugfiles -p$(curpass)
 
+	if test "$(curpass)" = "libc-bin"; then			\
+	  mv debian/$(curpass)/sbin/ldconfig			\
+	    debian/$(curpass)/sbin/ldconfig.real;		\
+	  install -m755 -o0 -g0 debian/local/ldconfig_wrap	\
+	    debian/$(curpass)/sbin/ldconfig;			\
+	fi
+
 	# extra_debhelper_pkg_install is used for debhelper.mk only.
 	# when you want to install extra packages, use extra_pkg_install.
 	$(call xx,extra_debhelper_pkg_install)
@@ -234,5 +241,6 @@ clean::
 	rm -f debian/*.lintian-overrides
 	rm -f debian/*.NEWS
 	rm -f debian/*.README.Debian
+	rm -f debian/*.triggers
 
 	rm -f $(stamp)binaryinst*
