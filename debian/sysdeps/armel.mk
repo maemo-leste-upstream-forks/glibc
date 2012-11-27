334df60c36778791c7183ecd5a633c7b04fb8be1
Adam Conrad <adconrad@0c3.net>
2012-11-27 23:46:48 +0000

    debian/sysdeps/{armel,armhf}.mk: Merge multilib configs from Ubuntu.
    svn path=/glibc-package/branches/eglibc-2.16/; revision=5422

--- debian/sysdeps/armel.mk
+++ debian/sysdeps/armel.mk
@@ -4,21 +4,27 @@ libc_add-ons = ports nptl $(add-ons)
 #DEB_ARCH_REGULAR_PACKAGES += libc6-armhf libc6-dev-armhf
 #armhf_add-ons = ports nptl $(add-ons)
 #armhf_configure_target = arm-linux-gnueabihf
-#armhf_CC = $(CC) -mfloat-abi=hard
-#armhf_CXX = $(CXX) -mfloat-abi=hard
+#armhf_CC = $(CC) -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard
+#armhf_CXX = $(CXX) -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard
 #armhf_slibdir = /lib/arm-linux-gnueabihf
 #armhf_libdir = /usr/lib/arm-linux-gnueabihf
 #
 #define libc6-dev-armhf_extra_pkg_install
+#
 #mkdir -p debian/libc6-dev-armhf/usr/include
-#cp -a debian/tmp-armhf/usr/include/bits \
-#	debian/libc6-dev-armhf/usr/include/
-#cp -a debian/tmp-armhf/usr/include/gnu \
-#	debian/libc6-dev-armhf/usr/include/
-#cp -a debian/tmp-armhf/usr/include/sys \
-#	debian/libc6-dev-armhf/usr/include/
-#cp debian/tmp-armhf/usr/include/fpu_control.h \
-#	debian/libc6-dev-armhf/usr/include/
+#ln -sf arm-linux-gnueabi/bits debian/libc6-dev-armhf/usr/include/
+#ln -sf arm-linux-gnueabi/gnu debian/libc6-dev-armhf/usr/include/
+#ln -sf arm-linux-gnueabi/fpu_control.h debian/libc6-dev-armhf/usr/include/
+#
+#mkdir -p debian/libc6-dev-armhf/usr/include/arm-linux-gnueabi/gnu
+#cp -a debian/tmp-armhf/usr/include/gnu/stubs-hard.h \
+#	debian/libc6-dev-armhf/usr/include/arm-linux-gnueabi/gnu
+#
+#mkdir -p debian/libc6-dev-armhf/usr/include/sys
+#for i in `ls debian/tmp-libc/usr/include/arm-linux-gnueabi/sys` ; do \
+#	ln -sf ../arm-linux-gnueabi/sys/$$i debian/libc6-dev-armhf/usr/include/sys/$$i ; \
+#done
+#
 #endef
 #
 #define libc6-armhf_extra_pkg_install
--- debian/sysdeps/armhf.mk
+++ debian/sysdeps/armhf.mk
@@ -15,21 +15,27 @@ endef
 #DEB_ARCH_REGULAR_PACKAGES += libc6-armel libc6-dev-armel
 #armel_add-ons = ports nptl $(add-ons)
 #armel_configure_target = arm-linux-gnueabi
-#armel_CC = $(CC) -mfloat-abi=softfp
-#armel_CXX = $(CXX) -mfloat-abi=softfp
+#armel_CC = $(CC) -mfloat-abi=soft
+#armel_CXX = $(CXX) -mfloat-abi=soft
 #armel_slibdir = /lib/arm-linux-gnueabi
 #armel_libdir = /usr/lib/arm-linux-gnueabi
 #
 #define libc6-dev-armel_extra_pkg_install
+#
 #mkdir -p debian/libc6-dev-armel/usr/include
-#cp -a debian/tmp-armel/usr/include/bits \
-#	debian/libc6-dev-armel/usr/include/
-#cp -a debian/tmp-armel/usr/include/gnu \
-#	debian/libc6-dev-armel/usr/include/
-#cp -a debian/tmp-armel/usr/include/sys \
-#	debian/libc6-dev-armel/usr/include/
-#cp debian/tmp-armel/usr/include/fpu_control.h \
-#	debian/libc6-dev-armel/usr/include/
+#ln -sf arm-linux-gnueabihf/bits debian/libc6-dev-armel/usr/include/
+#ln -sf arm-linux-gnueabihf/gnu debian/libc6-dev-armel/usr/include/
+#ln -sf arm-linux-gnueabihf/fpu_control.h debian/libc6-dev-armel/usr/include/
+#
+#mkdir -p debian/libc6-dev-armel/usr/include/arm-linux-gnueabihf/gnu
+#cp -a debian/tmp-armel/usr/include/gnu/stubs-soft.h \
+#	debian/libc6-dev-armel/usr/include/arm-linux-gnueabihf/gnu
+#
+#mkdir -p debian/libc6-dev-armel/usr/include/sys
+#for i in `ls debian/tmp-libc/usr/include/arm-linux-gnueabihf/sys` ; do \
+#	ln -sf ../arm-linux-gnueabihf/sys/$$i debian/libc6-dev-armel/usr/include/sys/$$i ; \
+#done
+#
 #endef
 #
 #define libc6-armel_extra_pkg_install
