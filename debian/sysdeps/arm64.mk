811f102b1110ea7204ced62caa3351046ef9834d
Adam Conrad <adconrad@0c3.net>
2013-02-06 21:20:51 +0000

    debian/sysdeps/arm64.mk: Remove --enable-multi-arch (LP: #1117602)
    svn path=/glibc-package/branches/eglibc-2.17/; revision=5483

--- debian/sysdeps/arm64.mk
+++ debian/sysdeps/arm64.mk
@@ -1,4 +1,4 @@
 libc_add-ons = ports nptl $(add-ons)
 
 # Passing --disable-profile until arm64 has profiling support
-extra_config_options = --enable-multi-arch --disable-profile
+extra_config_options = --disable-profile
