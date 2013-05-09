4432aad1fc768bf13f73d499a2c1ec48d3867aee
Adam Conrad <adconrad@0c3.net>
2013-05-09 09:59:50 +0000

    debian/sysdeps/ia64.mk, debian/control: Switch ia64 back to gcc-4.6, as our world explodes when compiled with gcc-4.7 on ia64 right now.
    svn path=/glibc-package/trunk/; revision=5569

--- debian/sysdeps/ia64.mk
+++ debian/sysdeps/ia64.mk
@@ -1,3 +1,7 @@
 libc_add-ons = ports nptl $(add-ons)
 
+# gcc-4.7 explodes on ia64, so use gcc-4.6 for now
+CC = $(DEB_HOST_GNU_TYPE)-$(BASE_CC)-4.6
+CXX = $(DEB_HOST_GNU_TYPE)-$(BASE_CXX)-4.6
+
 libc = libc6.1
