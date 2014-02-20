771878866d5085db38e5232c58e3f0a6b0c402ea
Adam Conrad <adconrad@0c3.net>
2014-02-20 09:14:39 +0000

    debian/sysdeps/powerpc.mk: Follow rename of stubs-64.h to stubs-64-v1.h.
    svn path=/glibc-package/branches/eglibc-2.19/; revision=5943

--- debian/sysdeps/powerpc.mk
+++ debian/sysdeps/powerpc.mk
@@ -21,7 +21,7 @@ ln -s powerpc-linux-gnu/gnu debian/libc6-dev-ppc64/usr/include/
 ln -s powerpc-linux-gnu/fpu_control.h debian/libc6-dev-ppc64/usr/include/
 
 mkdir -p debian/libc6-dev-ppc64/usr/include/powerpc-linux-gnu/gnu
-cp -a debian/tmp-ppc64/usr/include/gnu/stubs-64.h \
+cp -a debian/tmp-ppc64/usr/include/gnu/stubs-64-v1.h \
         debian/libc6-dev-ppc64/usr/include/powerpc-linux-gnu/gnu
 
 mkdir -p debian/libc6-dev-ppc64/usr/include/sys
