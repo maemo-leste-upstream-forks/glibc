3492b3b80a5ac1d9a43b001bc4a27671582350aa
Samuel Thibault <sthibault@debian.org>
2015-08-23 19:31:01 +0000

    backport r6480 from trunk: Fix stage1 about xen packages for hurd-i386 too.
    svn path=/glibc-package/branches/glibc-2.21/; revision=6481

--- debian/sysdeps/hurd-i386.mk
+++ debian/sysdeps/hurd-i386.mk
@@ -18,9 +18,11 @@ xen_extra_cflags = -march=i686 -mtune=generic -mno-tls-direct-seg-refs
 xen_slibdir = /lib/$(DEB_HOST_MULTIARCH)/i686/nosegneg
 xen_extra_config_options = $(extra_config_options)
 
+ifeq ($(filter stage1,$(DEB_BUILD_PROFILES)),)
 define libc0.3-dev_extra_pkg_install
 mkdir -p debian/libc0.3-dev/$(libdir)/xen
 cp -af debian/tmp-xen/$(libdir)/*.a \
 	debian/libc0.3-dev/$(libdir)/xen
 endef
+endif
 
