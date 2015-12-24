60606b3fadbbc3e97be52a4f1aa5076d22fd971b
Aurelien Jarno <aurel32@debian.org>
2015-12-24 16:52:56 +0000

    sysdeps/kfreebsd.mk: test if the header directory exists before trying to link directories from there. Fixes FTBFS on GNU/kFreeBSD.
    svn path=/glibc-package/trunk/; revision=6839

--- debian/sysdeps/kfreebsd.mk
+++ debian/sysdeps/kfreebsd.mk
@@ -43,12 +43,16 @@ $(stamp)mkincludedir:
 
 	mkdir -p debian/include/sys
 	# Link to any headers found in the old locations first
-	find $(KFREEBSD_HEADERS)/sys -mindepth 1 \
-		-exec ln -sf '{}' debian/include/sys ';'
+	if test -d $(KFREEBSD_HEADERS) ; then \
+	    find $(KFREEBSD_HEADERS)/sys -mindepth 1 \
+		-exec ln -sf '{}' debian/include/sys ';' ; \
+	fi
 	# Link to any headers found at the new multiarch location,
 	# replacing any existing links
-	find $(KFREEBSD_ARCH_HEADERS)/sys -mindepth 1 \
-		-exec ln -sf '{}' debian/include/sys ';'
+	if test -d $(KFREEBSD_ARCH_HEADERS) ; then \
+	    find $(KFREEBSD_ARCH_HEADERS)/sys -mindepth 1 \
+		-exec ln -sf '{}' debian/include/sys ';' ; \
+	fi
 
 	# To make configure happy if libc0.1-dev is not installed.
 	touch debian/include/assert.h
