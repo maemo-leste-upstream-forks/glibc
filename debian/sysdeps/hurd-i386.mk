8e5856c18cf268757b0a474e98cab6504812e45f
Samuel Thibault <samuel.thibault@ens-lyon.org>
2016-05-29 22:39:00 +0000

    sysdeps/hurd-i386.mk: Work around binutils bug with ifunc relocation assertion.

--- debian/sysdeps/hurd-i386.mk
+++ debian/sysdeps/hurd-i386.mk
@@ -12,3 +12,18 @@
 #	debian/libc0.3-dev/$(libdir)/xen
 #endef
 #endif
+
+# FIXME: We are having linking issues with ifunc...
+#
+# when linking libc_pic.os into libc.so:
+# /usr/bin/ld: /tmp/glibc-2.22/build-tree/hurd-i386-libc/libc_pic.os: warning: relocation against `_hurd_self_sigstate' in readonly section `.text'
+# /usr/bin/ld: read-only segment has dynamic IFUNC relocations; recompile with -fPIC
+# /usr/bin/ld: failed to set dynamic section sizes: Bad value
+# See
+# https://sourceware.org/bugzilla/show_bug.cgi?id=18801
+# https://sourceware.org/ml/libc-alpha/2016-05/msg00686.html
+# https://sourceware.org/bugzilla/show_bug.cgi?id=19939
+# supposed to be fixed in binutils 2.27
+#
+# For now we are just disabling ifunc:
+export libc_cv_ld_gnu_indirect_function=no
