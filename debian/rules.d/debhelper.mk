33ce9ca14aa66da831f6ef92a7ece0fa887b9775
Aurelien Jarno <aurelien@aurel32.net>
2016-09-03 21:08:47 +0200

    debian/rules.d/debhelper.mk: remove the arch-qualifier from the linux-libc-dev depends.  Closes: #836446.

--- debian/rules.d/debhelper.mk
+++ debian/rules.d/debhelper.mk
@@ -152,7 +152,7 @@ ifeq ($(filter stage2,$(DEB_BUILD_PROFILES)),)
 	echo 'libgcc:Depends=libgcc1 [!hppa !m68k], libgcc2 [m68k], libgcc4 [hppa]' >> tmp.substvars
 endif
 ifeq ($(DEB_HOST_ARCH_OS),linux)
-	echo "libc-dev:Depends=$(shell dpkg-query -f '$${binary:Package} (>= $${Version}) ' -W linux-libc-dev:$(DEB_HOST_ARCH))" >> tmp.substvars
+	echo "libc-dev:Depends=$$(dpkg-query -f '$${binary:Package} (>= $${Version}) ' -W linux-libc-dev:$(DEB_HOST_ARCH) | sed -e 's/:\S\+//')" >> tmp.substvars
 endif
 
 	for pkg in $(DEB_ARCH_REGULAR_PACKAGES) $(DEB_INDEP_REGULAR_PACKAGES) $(DEB_UDEB_PACKAGES); do \
