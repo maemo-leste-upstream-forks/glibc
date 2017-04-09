maemo-leste-upstream-forks/glibc

commit b3fd9db4d47ecef470be5b81557e996556df0a32

debian/rules.d/build.mk: do not fail the build in case of testsuite regressions when building a +debXuX version. That way changes in the environment (e.g.: kernel) do not prevent security or stable versions to be built.

debian/rules.d/build.mk
@@ -148,7 +148,9 @@ $(stamp)check_%: $(stamp)build_%
 	    echo "|     Encountered regressions that don't match expected failures.     |" ; \
 	    echo "+---------------------------------------------------------------------+" ; \
 	    grep -E '^FAIL:' $(DEB_BUILDDIR)/tests.sum | sort ; \
-	    exit 1 ; \
+	    if ! dpkg-parsechangelog | egrep -q '^Version:.*\+deb[0-9]+u[0-9]+' ; then
+	        exit 1 ; \
+	    fi ; \
 	  else \
 	    echo "+---------------------------------------------------------------------+" ; \
 	    echo "| Passed regression testing.  Give yourself a hearty pat on the back. |" ; \

commit 60a90313796317c3bc4fe37fcf9a1406a0940885

Fix two typos introduced in commit b3fd9db4d47ecef470be5b81557e996556df0a32

debian/rules.d/build.mk
@@ -148,9 +148,9 @@ $(stamp)check_%: $(stamp)build_%
 	    echo "|     Encountered regressions that don't match expected failures.     |" ; \
 	    echo "+---------------------------------------------------------------------+" ; \
 	    grep -E '^FAIL:' $(DEB_BUILDDIR)/tests.sum | sort ; \
-	    if ! dpkg-parsechangelog | egrep -q '^Version:.*\+deb[0-9]+u[0-9]+' ; then
+	    if ! dpkg-parsechangelog | egrep -q '^Version:.*\+deb[0-9]+u[0-9]+' ; then \
 	        exit 1 ; \
-	    fi ; \
+	    fi ; \
 	  else \
 	    echo "+---------------------------------------------------------------------+" ; \
 	    echo "| Passed regression testing.  Give yourself a hearty pat on the back. |" ; \