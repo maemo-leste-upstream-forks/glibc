e126e3ae332d560d0f942f0000eaa0f3a13f612d
Samuel Thibault <samuel.thibault@ens-lyon.org>
2017-09-07 11:30:59 +0200

    Refresh patch

--- debian/patches/hurd-i386/tg2.26-sigsetops.h
+++ debian/patches/hurd-i386/tg2.26-sigsetops.h
@@ -7,16 +7,14 @@ Subject: [PATCH] Include <sigsetops.h> from hurd/hurd/signal.h
 Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>
 
 ---
- hurd/hurd/signal.h | 5 +++++
+ hurd/hurd/signal.h |    5 +++++
  1 file changed, 5 insertions(+)
 
-diff --git a/hurd/hurd/signal.h b/hurd/hurd/signal.h
-index e03d53e6d7..c97699bcc9 100644
 --- a/hurd/hurd/signal.h
 +++ b/hurd/hurd/signal.h
-@@ -42,6 +42,11 @@
+@@ -41,6 +41,11 @@
+ #include <setjmp.h>		/* For `jmp_buf'.  */
  #include <spin-lock.h>
- #include <hurd/threadvar.h>	/* We cache sigstate in a threadvar.  */
  struct hurd_signal_preemptor;	/* <hurd/sigpreempt.h> */
 +#if defined __USE_EXTERN_INLINES && defined _LIBC
 +#  if IS_IN (libc)
@@ -26,5 +24,3 @@ index e03d53e6d7..c97699bcc9 100644
  
  
  /* Full details of a signal.  */
--- 
-tg: (854baea9b7..) t2.26/sigsetops.h (depends on: baseline-2.26)
