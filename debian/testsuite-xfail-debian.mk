fd1d25746115c7e57911de86f51e92b6bc45ac1c
Aurelien Jarno <aurelien@aurel32.net>
2018-07-16 21:16:59 +0200

    debian/testsuite-xfail-debian.mk: add reference testsuite results for riscv64.

--- debian/testsuite-xfail-debian.mk
+++ debian/testsuite-xfail-debian.mk
@@ -1038,6 +1038,54 @@ endif
 
 
 ######################################################################
+# riscv64
+# ######################################################################
+ifeq ($(config-machine)-$(config-os),riscv64-linux-gnu)
+test-xfail-test-double-isgreater = yes
+test-xfail-test-double-isgreaterequal = yes
+test-xfail-test-double-isless = yes
+test-xfail-test-double-islessequal = yes
+test-xfail-test-float-isgreater = yes
+test-xfail-test-float-isgreaterequal = yes
+test-xfail-test-float-isless = yes
+test-xfail-test-float-islessequal = yes
+test-xfail-test-float32-isgreater = yes
+test-xfail-test-float32-isgreaterequal = yes
+test-xfail-test-float32-isless = yes
+test-xfail-test-float32-islessequal = yes
+test-xfail-test-float32x-isgreater = yes
+test-xfail-test-float32x-isgreaterequal = yes
+test-xfail-test-float32x-isless = yes
+test-xfail-test-float32x-islessequal = yes
+test-xfail-test-float64-isgreater = yes
+test-xfail-test-float64-isgreaterequal = yes
+test-xfail-test-float64-isless = yes
+test-xfail-test-float64-islessequal = yes
+test-xfail-tst-cond-except = yes
+test-xfail-tst-cond24 = yes
+test-xfail-tst-cond25 = yes
+test-xfail-tst-execstack = yes
+test-xfail-tst-malloc-usable-tunables = yes
+test-xfail-tst-resolv-res_init = yes
+test-xfail-tst-resolv-res_init-thread = yes
+test-xfail-tst-resolv-threads = yes
+test-xfail-tst-robust-fork = yes
+test-xfail-tst-robust1 = yes
+test-xfail-tst-robust2 = yes
+test-xfail-tst-robust3 = yes
+test-xfail-tst-robust4 = yes
+test-xfail-tst-robust5 = yes
+test-xfail-tst-robust6 = yes
+test-xfail-tst-robust7 = yes
+test-xfail-tst-robust8 = yes
+test-xfail-tst-robust9 = yes
+test-xfail-tst-strfrom = yes
+test-xfail-tst-strfrom-locale = yes
+test-xfail-tst-tls12 = yes
+endif
+
+
+######################################################################
 # s390x
 ######################################################################
 ifeq ($(config-machine)-$(config-os),s390x-linux-gnu)
