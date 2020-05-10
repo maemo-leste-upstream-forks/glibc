66af8c57ba820bda5e5a7e33362d70b552c8fbff
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-05-10 22:50:17 +0000

    sysdeps/hurd-i386.mk: Move i386-specific mach headers to multiarch directory
    so they get accessible from the "machine" symlink of the gnumach-dev package.

--- debian/sysdeps/hurd-i386.mk
+++ debian/sysdeps/hurd-i386.mk
@@ -14,10 +14,13 @@ cp -af debian/tmp-xen/$(libdir)/*.a \
 endef
 endif
 
-# Glibc should really do this for us.
+# Glibc should really do this ld.so link for us.
 define libc_extra_install
 mkdir -p debian/tmp-$(curpass)/lib
 ln -s ld.so.1 debian/tmp-$(curpass)/lib/ld.so
+mkdir -p debian/tmp-$(curpass)/usr/include/$(DEB_HOST_MULTIARCH)/mach
+mv debian/tmp-$(curpass)/usr/include/mach/i386 debian/tmp-$(curpass)/usr/include/$(DEB_HOST_MULTIARCH)/mach/
+ln -s ../$(DEB_HOST_MULTIARCH)/mach/i386 debian/tmp-$(curpass)/usr/include/mach/i386
 endef
 
 # FIXME: We are having runtime issues with ifunc...
